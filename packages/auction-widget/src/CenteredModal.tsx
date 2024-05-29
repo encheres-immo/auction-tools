7db72f5644d0bcdb53a3ac5821b99c25fe52ed77
Goulven Clec'h <goulven.clech@protonmail.com>
2024-05-29 16:32:39 +0200

    fix & polish

--- packages/auction-widget/src/CenteredModal.tsx
+++ packages/auction-widget/src/CenteredModal.tsx
@@ -1,40 +1,38 @@
-import { Show, type Component, JSXElement } from 'solid-js';
-const CenteredModal: Component<{children: JSXElement, success: boolean | void, icon_class: string | void, title: string | void}> = (props: any) => {
-    return (
-    <div class="auction-widget-section">
+import { Show, type Component, JSXElement } from "solid-js";
+const CenteredModal: Component<{
+  children: JSXElement;
+  success: boolean | void;
+  icon_class: string | void;
+  title: string | void;
+}> = (props: any) => {
+  return (
+    <div>
+      <div id="auction-widget-modal-background"></div>
+
       <div class="auction-widget-modal">
-
-        <div id="auction-widget-bg">
-          <div id="auction-widget-bg-color" />
-        </div>
-
         <div id="auction-widget-modal-content">
           <div>
-              <Show when={props.success}>
-                  <div class="auction-widget-icon">
-                    <i class="fas fa-check" />
-                  </div>
-              </Show>
-              <Show when={props.icon_class}>
-                  <div class="auction-widget-icon">
-                      <i class={props.icon_class} />
-                  </div>
-              </Show>
+            <Show when={props.success}>
+              <div class="auction-widget-icon">
+                <i class="fas fa-check" />
+              </div>
+            </Show>
+            <Show when={props.icon_class}>
+              <div class="auction-widget-icon">
+                <i class={props.icon_class} />
+              </div>
+            </Show>
             <div>
               <Show when={props.title}>
-                <h3>
-                  {props.title}
-                </h3>
+                <h3>{props.title}</h3>
               </Show>
-              <div>
-                {props.children}
-              </div>
+              <div>{props.children}</div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    )
+  );
 };
 
 export default CenteredModal;
